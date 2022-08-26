0e3df23a0ed4bec11d872532e3450f996adba24c
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-08-26 13:46:29 +0300

    complete the deliverables

--- variables-and-data-types/app.rb
+++ variables-and-data-types/app.rb
@@ -1,11 +1,20 @@
 # Variables and Data Types
 ## Q1 Set variables for the corresponding values:
-
+number = 25
+puts"#{number}"
 #a number to the value 25
+dog = "Snoopy"
+dog == "Snoopy"
+puts result = dog == "Snoopy"
 #a boolean to the value true 
+language_name = "Ruby"
+puts "#{language_name}"
 #a string to the value "Ruby"
 
 ##  Q2 Set values 100, 30 for the  variables cars and drivers respectively.
 #Output: There are 100 cars and 30 drivers.
+var1 = 100
+var2 = 30
+puts "There are #{var1} cars and #{var2} drivers"
 
 
